5a91fab5e92be9d4170858182de4cd57ea4d8d14
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-03-18 03:43:51 +0200

    Done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int n;
 
-	for(n = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
-	for(n = 97; n < 103; n++)
+	for (n = 97; n < 103; n++)
 	{
 		putchar(n);
 	}
