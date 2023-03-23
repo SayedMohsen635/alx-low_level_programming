979dd94abc9a22e8d3a22bc57da4374204b8f433
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-03-23 03:12:12 +0200

    Done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-* main - Entry point
-* print_alphabet - The function is used to print all
-* alphabets from A to Z
-* Return: Always 0 (Success)
+ * print_alphabet - The function is used to print all
+ * alphabets from A to Z
+ * Return: Always 0 (Success)
 */
 
 void print_alphabet(void)
