9d450a6d441e293e15e2b1816cde6578e0745400
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-06 18:02:04 +0200

    Done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 {
 	int length = 0;
 	
-	if (s != '\0')
+	if (*s != '\0')
 	{
 		length += _strlen_recursion(s + 1) + 1;
 	}
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,5 +9,8 @@
 
 int factorial (int n)
 {
-	return n * factorial(n - 1);
+	if (n > 0)
+	{
+		return n * factorial(n - 1);
+	}
 }
