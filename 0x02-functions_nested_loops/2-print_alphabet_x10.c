b53190ddf44d443b0f83f9e1b8c67793f151bdf7
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-07 14:11:33 +0200

    Done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this function prints the alphabet in lowercase 10 times
+ * Return: 1 (Successful)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%s", "abcdefghijklmnopqrstuvwxyz");
+		if (i != 9)
+		{
+			printf("%c", '\n');
+		}
+	}
+}
