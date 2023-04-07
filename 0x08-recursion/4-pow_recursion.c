6a1bed6980ed21118f4a688276cdb1825f02c9e7
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-07 02:57:54 +0200

    Done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,12 +10,16 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y > 0)
 	{
-		return (-1);
+		return (_pow_recursion(x, y - 1));
+	}
+	else if (y == 0)
+	{
+		return (1);
 	}
 	else
 	{
-		return (_pow_recursion(x, y - 1));
+		return (-1);
 	}
 }
