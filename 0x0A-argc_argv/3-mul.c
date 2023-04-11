4c2334782c9c730c070e14401d4a3011df1728c1
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-11 04:26:08 +0200

    Done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
 	int result;
 
-	if (argc == 1)
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it adds all positive numbers
+ * @argc: int
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+	}else if ()
+}
