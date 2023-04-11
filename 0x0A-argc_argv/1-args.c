e6748836b5b08e681833d49c126c959a74933c9f
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-11 02:36:38 +0200

    Done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this file prints number of args passed into it
+ * @argc: int
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
