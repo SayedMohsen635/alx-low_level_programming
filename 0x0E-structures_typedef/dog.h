4bcae6fc8a3f849a6c9a20555cc2d87d4cb4c9b6
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-24 00:30:11 +0200

    Done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
