5f170d15b6f2442d90f33370bfe54b66d03dea45
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-04-25 00:08:26 +0200

    Done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * main - runs the program
+ * @argc: number of args
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int (*opFunc)(int, int), a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	opFunc = get_op_func(argv[2]);
+	if (!opFunc)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
