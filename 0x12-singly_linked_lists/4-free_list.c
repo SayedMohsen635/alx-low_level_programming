3d12c61d0a32b678a8587290cbb1beecd3fdd83f
SayedMohsen <sayedmohsen635@gmail.com>
2023-04-28 03:47:50 +0300

    Done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees nodes on a list
+ * @head: pointer to a head
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node, *nextNode;
+
+	if (!head)
+		return;
+	
+	node = head;
+	while (node)
+	{
+		nextNode = node->next;
+		free(node->str);
+		free(node);
+		node = nextNode;
+	}
+}
