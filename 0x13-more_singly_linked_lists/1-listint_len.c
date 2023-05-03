31007505298ef76c53b5a930f4519cc308c8ed45
SayedMohsen <sayedmohsen635@gmail.com>
2023-05-03 05:28:38 +0300

    Done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list.
+ * listint_len - prints all the elements of a listint_t list.
  * @h: pointer to struct
  * Return: number of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
