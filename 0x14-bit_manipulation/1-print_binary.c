ede7fe95a257755ae1e96fc347068170f48a2217
SayedMohsen <sayedmohsen635@gmail.com>
2023-05-05 03:42:55 +0300

    Done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,5 +8,19 @@
 
 void print_binary(unsigned long int n)
 {
-	printf("%b", n);
+	int b = 8 * sizeof(n);
+	int printed = 0;
+
+	while (b)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			printed++;
+		}
+		else
+			_putchar('0);
+	}
+	if (!printed)
+		_putchar('0);
 }
