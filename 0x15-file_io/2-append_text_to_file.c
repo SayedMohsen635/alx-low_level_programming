281b5b28cf1a64eecae603389bad73aeb4d29f58
SayedMohsen <sayedmohsen635@gmail.com>
2023-05-14 01:23:55 +0300

    Done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -35,7 +35,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fileVal = open(filename, O_WRONLY | O_APPEND);
 	if (fileVal == -1)
 		return (-1);
-	if (write(fileVal, text_content, strLength(text_content)) == -1);
+	if (write(fileVal, text_content, strLength(text_content)) == -1)
 		return (-1);
 	close(fileVal);
 	return (1);
