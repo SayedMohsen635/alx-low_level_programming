7323706cbf68ef0a00c73d0d4856514f113b559d
SayedMohsen <sayedmohsen635@gmail.com>
2023-05-14 14:08:01 +0300

    Done

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main - This program that copies the content of a file to another file
+ * @argc: int
+ * @argv: array of pointers
+ * Return: 1 or 0
+ */
+
+int main(int argc, char **argv)
+{
+	int file_from = 0, file_to = 0;
+	ssize_t bytes;
+	char buffer[BUFF_SIZE];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]), exit(98);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
+	/* Write Logic */
+	while ((bytes = read(file_from, buffer, BUFF_SIZE)) > 0)
+	{
+		if (write(file_to, buffer, bytes) != bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			exit(99);
+		}
+	}
+	/* Handle Read Logic Error*/
+	if (bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+		exit(98);
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to);
+		exit(100);
+	}
+	return (0);
+}
