3f58b8e618e814ed836d4a0c856de885190d32a9
SayedMohsen635 <sayedmohsen635@gmail.com>
2023-08-03 01:47:30 +0300

    Done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - this function prints the length of a string
+ * @s: Pointer to the string
+ * Return: Always 0 (Successful)
+ * Failed: -1
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		length += _strlen_recursion(s + 1) + 1;
+	}
+
+	return (length);
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * factorial - this function returns the factorial of a number
+ * @n: integer number
+ * Return: Always 0 (Successful)
+ * Failed: -1
+ */
+
+int factorial(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		result = n * factorial(n - 1);
+	}
+	else if (n < 0)
+	{
+		result = -1;
+	}
+	else
+	{
+		result = 1;
+	}
+	return (result);
+}
